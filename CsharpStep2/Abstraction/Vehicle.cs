ad04061f0604d908b52cc6a886726793f48f36d7
Chanda Mounica <pvryar.4@gmail.com>
2025-07-09 15:27:38 +0530

    abstract class and implementation

---
+++ CsharpStep2/Abstraction/Vehicle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace vehicleConfigurationSystem
+{
+    //rule+Implement method
+    abstract class Vehicle
+    {
+
+        //Methods with out a body and implemented in derived class(child class)
+        //abstract method without body
+        //public abstract void StartEngine();
+        ////concrete method with body
+        //public void StopEngine()
+        //{
+        //    Console.WriteLine("Engine stopped.");
+        //}
+        //rules = stop();
+        //start();
+
+        public abstract void start();
+        public abstract void stop();
+        public abstract void Engine();
+
+        //properties
+        public string FuelType {get; set;}
+        public int Capacity { get; set; }
+
+        //concrete method
+
+        public void showDetails()
+        {
+            Console.WriteLine($"fuel Type: {FuelType}");
+            Console.WriteLine($"Capacity: {Capacity} passengers");
+
+        }
+
+    }
+
+
+    class Car : Vehicle
+    {
+        //we uese override keyword in csharp  when we implement abstract method in derived class(if you remove overide it acts as direct call)
+        public override void Engine()
+        {
+            Console.WriteLine($"car engine has started with 4 cyclinder engine");
+        }
+
+        public override void start()
+        {
+            Console.WriteLine("Car has started.");
+        }
+
+        public override void stop()
+        {
+            Console.WriteLine("Car has stopped.");
+        }
+
+        //direct method
+        public void CarPurchaseDateandTime()
+        {
+            Console.WriteLine("car purshased on 2012-08-49 at 10:00 AM");
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override void Engine()
+        {
+            Console.WriteLine("Bike engine start with 2 stroke");
+        }
+
+        public override void start()
+        {
+            Console.WriteLine("Bike engine start");
+        }
+
+        public override void stop()
+        {
+            Console.WriteLine("Bike engine stop");
+        }
+    }
+
+    class program
+    {
+        static void Main()
+        {
+            //can't create Instance for abstract class
+            //Vehicle myVehicle = new Vehicle(); //error: cannot create instance of abstract class
+
+            //Vehicle carvehicle = new Car();
+            //carvehicle.start();
+            //carvehicle.stop();
+            //carvehicle.Engine();
+            //carvehicle.FuelType = "Diesel";
+            //carvehicle.Capacity = 5;
+            //carvehicle.showDetails();
+
+            Vehicle carvehicle = new Car()
+            {
+                FuelType = "Petrol",
+                Capacity = 3
+            };
+            carvehicle.showDetails();
+            carvehicle.start();
+            carvehicle.stop();
+            carvehicle.Engine();
+
+            Vehicle bikevehicle = new Bike()
+            {
+                FuelType = "Petrol",
+                Capacity = 2
+            };
+            bikevehicle.start();
+            bikevehicle.stop();
+            bikevehicle.Engine();
+            bikevehicle.showDetails();
+
+        }
+    }
+}
---
+++ CsharpStep2/Implementation/TVRemoteControlinterface.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace RemoteControlSystems
+{
+    //can create multiple interfaces.Rules provided in inteerface. we only define properties and methods,
+    //we can't implement them.
+
+    interface Iremote
+    {
+        //interface provide rules and wouldn't implement.
+        void PowerOn();
+        void PowerOff();
+        void ChangeChannel(int channelNumber);
+        void Increasevolume();
+
+    }
+    interface IRemotePrice
+    {
+        string getPriceDetails();
+
+    }
+
+    interface IRemoteLocationDetails
+    {
+        string getLocation();
+    }
+
+    //I would like to take contract rules from Iremote. In class we need to implement rules.
+    class SonyRemote : Iremote, IRemotePrice //(ctrl+.)
+    {
+        public string getPriceDetails()
+        {
+            return "500Rupees";
+        }
+        public void ChangeChannel(int channelNumber)
+        {
+            Console.WriteLine($"Sony Tv changed to channel {channelNumber}");
+        }
+        public void Increasevolume()
+        {
+            Console.WriteLine("Sony Tv Increase Volume");
+        }
+
+        public void PowerOff()
+        {
+            Console.WriteLine("Sony Tv PowerOff");
+        }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("Sony Tv PowerOn");
+        }
+    }
+
+    class LGRemote : Iremote, IRemoteLocationDetails
+    {
+        public void ChangeChannel(int channelNumber)
+        {
+            Console.WriteLine($"LG Tv changed to channel {channelNumber}");
+        }
+
+        public string getLocation()
+        {
+            return "Hyderabad, India";
+        }
+
+        public void Increasevolume()
+        {
+            Console.WriteLine("LG Tv Increase Volume");
+        }
+
+        public void PowerOff()
+        {
+            Console.WriteLine("LG Tv PowerOff");
+        }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("LG Tv PowerOn");
+        }
+    }
+
+    class program
+    {
+        static void main()
+        {
+            Iremote lgremote = new LGRemote();
+            //here lgremote is not an instance of class. It is reference of interface
+            //Iremote is contract of rules and new LGRemote is which is followed by the rules.
+            lgremote.ChangeChannel(2);
+            lgremote.PowerOn();
+
+            Iremote sonyremote = new SonyRemote();
+            sonyremote.ChangeChannel(9);
+            sonyremote.PowerOff();
+
+            IRemotePrice remotePrice = new SonyRemote();
+            //remotePrice.getPriceDetails();
+            string price = remotePrice.getPriceDetails();
+            Console.WriteLine(price);
+
+            IRemoteLocationDetails remoteLocationDetails = new LGRemote();
+            //remoteLocationDetails.getLocation();
+            string location = remoteLocationDetails.getLocation();
+            Console.WriteLine(location);
+
+
+
+        }
+    }
+}
