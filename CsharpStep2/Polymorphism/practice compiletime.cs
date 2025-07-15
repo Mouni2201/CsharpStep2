75d35c0b465d5f082f0ce77c1954ff2dc34c1e2d
Chanda Mounica <pvryar.4@gmail.com>
2025-07-15 13:51:46 +0530

    practice

---
+++ CsharpStep2/Polymorphism/practice compiletime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace CompileTimePolymorphism
+
+{
+    //Changing the number of Parameters
+    class CompileParamter
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Sum of two integers: " + (a + b));
+        }
+        public void Add(int a, int b, int c)
+        {
+            Console.WriteLine("Sum of three integers: " + (a * b * c));
+        }
+        
+        
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            CompileParamter p = new CompileParamter();
+             p.Add(3, 4);
+
+            //Console.WriteLine($"Sum is: {Sum1}");
+           
+        }
+    }
+
+
+}
